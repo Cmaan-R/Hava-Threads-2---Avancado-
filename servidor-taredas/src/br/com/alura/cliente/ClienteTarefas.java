f26ba35aa06b05d33bcd3733bebe851fde7a4473
CMAAN-R <robsonleandrox@hotmail.com>
2023-01-16 09:20:19 -0300

    Update

---
+++ servidor-taredas/src/br/com/alura/cliente/ClienteTarefas.java
@@ -0,0 +1,18 @@
+package br.com.alura.cliente;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClienteTarefas {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		Socket socket = new Socket("localhost", 12345);
+
+		System.out.println("Conexão estabelecida");
+
+		socket.close();
+	}
+
+}
